3fb2b075b890b59dd1823681ad30c9cbb7f9f9c1
Hà Anh Tuấn <tuancntt971@gmail.com>
2023-02-10 13:53:53 +0700

    add draft support ubuntu 18.04, 20.04, 22.04

---
+++ scripts/ubuntu/20.04/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#######################################################
+# LempVPS Script v2.0.4 for Ubuntu 20.04
+# To install type: 
+# curl -sO https://sys.ufaceplus/scripts/$(rpm -E %ubuntu)/install && bash install
+#######################################################
